06f90709363807624707c7cb2d5195b33716d1b0
Fabio Lima <fllima@firjan.com.br>
2019-06-26 12:08:50 -0300

    Versionando o Primeiro projeto em React Js

---
+++ src/componentes/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./styles.css";
+//criando componentes como função, substituindo
+//class Header extends Component {
+    //render(){
+        //return ...
+    //}
+//}
+const Header = () => 
+  <header id="main-header">Js Home</header>;
+
+
+export default Header;
---
+++ src/pages/main/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import api from "../../services/api";
+
+import './styles.css';
+
+export default class Main extends Component{
+
+    state = {
+        products: [],
+    }
+
+    componentDidMount(){
+        this.loadProducts();
+    }
+
+    loadProducts = async () => {
+        const response = await api.get('/products');
+
+        //console.log(response.data.docs);
+        this.setState({ products: response.data.docs });
+
+    };
+
+    render(){
+
+        const {products} = this.state;
+        //return <h3>Contagem de produtos: {this.state.products.length}</h3>;
+        return (
+            <div className="product-list">
+                {products.map(product => (
+                    <article key={product._id}>
+                        <strong>{product.title}</strong>
+                        <p>{product.description}</p>
+
+                        <a href="">Acessar</a>
+                    </article>
+                    //<h3 key={product._id}>{product.title}</h3>
+                ))}
+            </div>
+        );
+    }
+}
